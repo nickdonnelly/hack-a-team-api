58f26c31382c31c9521006dc7908d515021e6645
Nicholas Donnelly <ndonnelly1006@gmail.com>
2017-02-21 14:16:26 +0100

    Added very basic structures for trivial errors.

--- app/controllers/api_controller_controller.rb
+++ app/controllers/api_controller_controller.rb
@@ -1,2 +1,14 @@
 class ApiControllerController < ApplicationController
+
+    def unknown
+        render json: "[0a] Unknown API endpoint."
+    end
+
+    def nokey
+        render json: "[0b] No API key provided."
+    end
+
+    def badkey
+        render json: "[0c] Invalid API key provided."
+    end
 end
