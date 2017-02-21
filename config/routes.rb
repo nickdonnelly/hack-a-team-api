2a549a488a1cdf202e714b7cf29da927190a1116
Nicholas Donnelly <ndonnelly1006@gmail.com>
2017-02-21 14:17:27 +0100

    Basic shell for api endpoints in routes file added.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'api#unknown'
+
+  scope '/api' do
+    scope '/v1' do
+      # Endpoints start here.
+    end
+  end
 end
