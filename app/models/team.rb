ed73a4e806529a58a2686263b33cf96c573eae51
Nicholas Donnelly <ndonnelly1006@gmail.com>
2017-02-26 11:08:13 +0100

    Added edit api endpoint for teams.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,18 @@
 class Team < ApplicationRecord
+  serialize :members, Array
+
+
+  validates :team_name, presence: true
+  validates :team_image, presence: true
+  validates :team_link , presence: true
+  validates :video_link, presence: true
+  validates :description, presence: true
+  validates :challenge_id, presence: true
+  validates :contact_email, presence: true
+  validates :contact_phone, presence: true
+  validates :members, presence: true, length: { maximum: 5 }
+
+
+  private
+
 end
