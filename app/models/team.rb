64dac164a842e96cc655034a4f3320e7584fcbd3
Nicholas Donnelly <ndonnelly1006@gmail.com>
2017-03-07 11:37:10 +0100

    Allowed blank things for certain fields in team model.

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,15 +2,15 @@ class Team < ApplicationRecord
   serialize :members, Array
 
 
-  validates :team_name, presence: true
-  validates :team_img, presence: true
-  validates :team_link , presence: true
-  validates :video_link, presence: true
-  validates :description, presence: true
-  validates :challenge_id, presence: true
-  validates :contact_email, presence: true
-  validates :contact_phone, presence: true
-  validates :members, presence: true, length: { maximum: 5 }
+  validates :team_name, presence: true, length: {minimum: 3, maximum: 24}, allow_blank: false
+  validates :team_img, presence: true, allow_blank: false
+  validates :team_link , presence: true, allow_blank: true
+  validates :video_link, presence: true, allow_blank: true
+  validates :description, presence: true, allow_blank: true
+  validates :challenge_id, presence: true, allow_blank: false
+  validates :contact_email, presence: true, allow_blank: false
+  validates :contact_phone, presence: true, allow_blank: true
+  validates :members, presence: true, length: { maximum: 5 }, allow_blank: false
   # TODO: regex validations for each of these fields.
 
   private
