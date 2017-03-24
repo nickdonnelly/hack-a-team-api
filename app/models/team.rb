cf0e4ebe727deb1aabc83db19a3d78cefccca978
Nicholas Donnelly <ndonnelly1006@gmail.com>
2017-03-24 15:27:31 +0100

    Removed some validations for teams.

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,12 +4,9 @@ class Team < ApplicationRecord
 
   validates :team_name, presence: true, length: {minimum: 3, maximum: 24}, allow_blank: false
   validates :team_img, presence: true, allow_blank: false
-  validates :team_link , presence: true, allow_blank: true
-  validates :video_link, presence: true, allow_blank: true
   validates :description, presence: true, allow_blank: true
   validates :challenge_id, presence: true, allow_blank: false
   validates :contact_email, presence: true, allow_blank: false
-  validates :contact_phone, presence: true, allow_blank: true
   validates :members, presence: true, length: { maximum: 5 }, allow_blank: false
   # TODO: regex validations for each of these fields.
 
