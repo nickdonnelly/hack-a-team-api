720df60b20c1d0487898cfea28e366abdb2e3f97
Nicholas Donnelly <ndonnelly1006@gmail.com>
2017-03-24 15:50:01 +0100

    Fixed migration.

--- db/migrate/20170324143259_rework_first_login_users.rb
+++ db/migrate/20170324143259_rework_first_login_users.rb
@@ -1,6 +1,6 @@
 class ReworkFirstLoginUsers < ActiveRecord::Migration[5.0]
   def change
     remove_column :users, :first_login
-    add_column :users, :first_login, :boolean
+    add_column :users, :first_login, :boolean, default: true
   end
 end
